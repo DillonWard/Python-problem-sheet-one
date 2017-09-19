b674fe1918dbfa8a04cceeac3e2f8be886b6010e
Dillon Ward <Dillonward13@gmail.com>
2017-09-19 16:26:12 +0100

    fizzbuzz problem solved

---
+++ fizzbuzz.py
@@ -0,0 +1,21 @@
+# loops between the range of 1 and 101
+for i in range(1, 101):
+
+    # if 'i' being divisible by 3 and 5 is true, print out FizzBuzz 
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+        # check for both 3 and 5 first
+        # otherwise it'll print out 'Fizz' or 'Buzz' when 'i' reaches 15
+        # depending on which comes first in the if statement
+
+    # if 'i' being divisible by 3 is true, print out Fizz
+    elif i % 3 == 0:
+        print ("Fizz")
+
+    # if 'i' being divisible by 5 is true, print out Buzz
+    elif i % 5 == 0:
+        print("Buzz")
+    
+    # otherwise, print out 'i'
+    else:
+        print(i)
