c1cdc063bd5caaf28d750da05a83676830326eb8
Dillon Ward <Dillonward13@gmail.com>
2017-09-28 12:25:53 +0100

    reverse string problem solved

---
+++ reverse-string.py
@@ -0,0 +1,12 @@
+# function to reverse a string
+# Author: Dillon Ward (Dillonward2017@gmail.com)
+# Date: 25/09/2017
+
+# the user enters a string to be reversed
+sentence = input("Enter a sentence/word:")
+
+# the string is reversed and is assigned to 'reversed_string'
+reversed_string = sentence[::-1]
+
+# the new reversed string is printed out
+print(reversed_string)
